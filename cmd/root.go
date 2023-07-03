1c16f24484a594ac7de3a5b3dd1dc689c309461c
cyberkarma <icegroovekid@gmail.com>
2023-07-03 18:48:27 +0500

    initial commit

---
+++ cmd/root.go
@@ -0,0 +1,19 @@
+package cmd
+
+import (
+	"os"
+
+	"github.com/rs/zerolog/log"
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Use: "app",
+}
+
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		log.Fatal().Err(err).Msg("execute root cmd")
+		os.Exit(1)
+	}
+}
---
+++ src/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/cyberkarma/chatserver/cmd"
+
+func main() {
+	cmd.Execute()
+}
