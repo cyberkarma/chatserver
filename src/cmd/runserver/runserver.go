1c88c22315f0f79a6009102b069455dbbca9355d
cyberkarma <icegroovekid@gmail.com>
2023-07-14 11:54:05 +0500

    run server function

--- src/cmd/runserver/runserver.go
+++ src/cmd/runserver/runserver.go
@@ -2,15 +2,19 @@ package runserver
 
 import (
 	"fmt"
+	"github.com/cyberkarma/chatserver/server"
 	"github.com/spf13/cobra"
 	_ "github.com/spf13/cobra"
+	"net/http"
 )
 
 var RunServer = &cobra.Command{
-	Use:   "printCmd21",
-	Short: "Run the print",
+	Use:   "runServer",
+	Short: "Run the server",
 	Long:  "Blablabla",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("Print is Running")
+		router := server.RouterBuilder{}
+		http.ListenAndServe(":3000", router.Build())
+		fmt.Println("Server is working now")
 	},
 }
